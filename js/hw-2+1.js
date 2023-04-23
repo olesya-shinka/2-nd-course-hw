7bc44bc5bb0ec96728162292df910d9bc2d9b50b
Олеся Шинка <os1124856@gmail.com>
2023-04-23 12:23:07 +0500

    change styles

--- js/hw-2+1.js
+++ js/hw-2+1.js
@@ -25,10 +25,10 @@ let newresult = f % g;
 alert (newresult);
 
 let num = 1;
-num = num + 5;
-num = num - 3;
-num = num * 7;
-num = num / 3;
-num = num + 1;
-num = num - 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+++num;
+--num;
 alert(num);
