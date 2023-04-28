43acd941118c5de1418f475e43a1818d837d395c
Олеся Шинка <os1124856@gmail.com>
2023-04-28 13:03:23 +0500

    changed js

---
+++ js/hw-3.js
@@ -0,0 +1,43 @@
+//задание 1
+let password = 'skypro';
+let answer = prompt ('Введите пароль');
+if (password === '123') {
+    alert ('Пароль введен верно');
+} else {
+    alert ('Пароль введен неправильно')
+};
+//задание 2
+let c = 5;
+c = 0 && 10 && -3 && 2;
+alert (c > 0); 
+alert (c < 10);
+alert (c <= 10);
+alert (c >= 0);
+//задание 3
+let d = 50;
+let e = 101;
+if (d < 100) {
+    alert('Верно')
+} else {
+    alert('Неверно')
+};
+if (e > 100) {
+    alert('Верно')
+} else {
+    alert('Неверно')
+};
+//задание 4
+let a = 2;
+let b = 3;
+alert(a + b);
+//задание 5
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
+
